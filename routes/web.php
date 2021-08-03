179f68bd8ad4fdceff032186172794e305ed760f
Bogdan <bigden313313@mail.ru>
2021-08-03 15:08:41 +0300

    laravel

---
+++ routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+
+use App\Task;
+use Illuminate\Http\Request;
+
+Route::get('/home', function () {
+    return view('index');
+});
+
+Route::get('/menu', function () {
+    return view('menu');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/elements', function () {
+    return view('elements');
+});
+
+Route::get('/single-blog', function () {
+    return view('single-blog');
+});
+
+/**Route::get('/', function () {
+    return view('welcome');
+});*/
